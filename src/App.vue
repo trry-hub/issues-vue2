907588c47bb2e297e090329f7408eecde14077ad
shizhu <shizhu@yaomaitong.cn>
2024-07-22 09:05:24 +0800

    测试 swiper

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="swiper-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(slide, index) in slides" :key="index">
+        <div><img :src="require(`@/assets/images/${slide.image}`)" /></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Swiper from 'swiper';
+import { EffectCards, Autoplay } from 'swiper/modules'
+import 'swiper/css';
+import 'swiper/css/effect-cards';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      slides: [
+        { image: "pg2.png" },
+        { image: "pg3.png" },
+        { image: "pg1.png" },
+        { image: "pg4.png" },
+        { image: "pg2.png" },
+        { image: "pg3.png" },
+      ],
+    };
+  },
+  mounted() {
+    let cards = {
+      effect: "cards",
+      resistanceRatio: 0,
+      loop: true,
+      autoplay: true,
+      cardsEffect: {
+        slideShadows: false,
+        rotate: false,
+      },
+    };
+    Swiper.use([EffectCards, Autoplay]);
+    new Swiper(".swiper-container", cards);
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.swiper-container {
+  width: 40%;
+  margin: 0 auto;
+}
+
+.swiper-slide {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.swiper-slide div img {
+  width: 100%;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  configureWebpack: {
+    resolve: {
+      alias: {
+        // BASE_CONFIG: configPath,
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+  },
   transpileDependencies: true
 })
