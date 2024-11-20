fe7d21b21600f734cd2b747dc20fe89dbce428cb
shizhu <shizhu@yaomaitong.cn>
2024-11-20 18:53:45 +0800

    feat(cert): 添加证书文件并更新项目依赖
    - 新增 cert.crt 和 cert.key 文件用于项目加密
    - 更新 package-lock.json 和 package.json，添加新依赖项
    - 移除 App.vue 中的 HelloWorld 组件引用

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require('fs');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: {
+      key: fs.readFileSync(path.join(__dirname, 'cert', 'cert.key')),
+      cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.crt')),
+    },
+  }
 })
